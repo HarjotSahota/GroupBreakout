b66021d40ba1a041b0dc70204630b088573d5e98
HarjotSahota <93726984+HarjotSahota@users.noreply.github.com>
2025-03-18 10:53:46 -0700

    Implemented Score feature
    - [x] every time brick is hit the score is implemented by 1
    - [x] score has an animation
    - [x] score feature is on the top right of the player screen

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int maxLives = 3;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+    //public ParticleSystem ExplosionParticles; 
+    //public ParticleSystem FlashParticles;
+
+
+    private int brick = 0;
+    [SerializeField] private BrickCounterUI brickCounter;
 
     private int currentBrickCount;
     private int totalBrickCount;
@@ -24,14 +31,22 @@ public class GameManager : SingletonMonoBehavior<GameManager>
 
     private void FireBall()
     {
+        //AudioManager.instance.PlaySfx("Fire");
         ball.FireBall();
     }
 
     public void OnBrickDestroyed(Vector3 position)
     {
         // fire audio here
+        //AudioManager.instance.PlaySfx("Brick");
         // implement particle effect here
+        //PlayHitEffects(position);
         // add camera shake here
+
+        // implementing coin text
+        brick ++;
+        brickCounter.UpdateScore(brick);
+
         currentBrickCount--;
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
         if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
@@ -41,7 +56,17 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     {
         maxLives--;
         // update lives on HUD here
+        //AudioManager.instance.PlaySfx("Miss");
         // game over UI if maxLives < 0, then exit to main menu after delay
         ball.ResetBall();
     }
-}
+
+/*
+    public void PlayHitEffects(Vector3 position){
+        ExplosionParticles.transform.position = position;
+        FlashParticles.transform.position = position;
+        ExplosionParticles.Play();
+        FlashParticles.Play();
+    }
+*/
+}   
